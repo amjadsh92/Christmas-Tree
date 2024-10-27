6c71e0ff683b649de57159cf04b7fd31bf981243
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-27 23:21:25 +0200

    Organize the code

--- main.js
+++ main.js
@@ -1,114 +1,69 @@
-var n = prompt("Enter a number")
+var n = prompt("Enter a number");
 
+var length = 1 + 2 * (n - 1);
 
+function space(n) {
+  var wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "wrapper");
+  for (let i = 0; i < n; i++) {
+    var spacespan = document.createElement("span");
+    wrapper.appendChild(spacespan);
+  }
 
-var length = 1 + 2 * (n - 1)
-
-function space (n){
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute('class', "wrapper")
-    for (let i = 0; i < n; i++){
-        
-        var spacespan = document.createElement("span");
-        wrapper.appendChild(spacespan);
-
-    
-    
-
-     }
-
-     return wrapper
+  return wrapper;
 }
 
+function line(n) {
+  var wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "wrapper");
+  for (let i = 0; i < n; i++) {
+    var linespan = document.createElement("span");
+    linespan.setAttribute("class", "line");
+    var textnode = document.createTextNode("|");
+    linespan.appendChild(textnode);
+    wrapper.appendChild(linespan);
+  }
 
-
-function line (n){
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute('class', "wrapper")
-    for (let i = 0; i < n; i++){
-        
-        var linespan = document.createElement("span");
-        linespan.setAttribute("class", "line")
-        var textnode = document.createTextNode('|')
-        linespan.appendChild(textnode)
-        wrapper.appendChild(linespan);
-
-    
-    
-
-     }
-
-     return wrapper
+  return wrapper;
 }
 
 body = document.getElementById("body");
-maindiv = document.createElement("div")
-maindiv.setAttribute("class", "tree")
+maindiv = document.createElement("div");
+maindiv.setAttribute("class", "tree");
 
-for (let i = 0; i < n; i++){
-    //empty =" ".repeat((length - (2 * i + 1)) / 2)
-    
-    if (i == 0){
-       
-       
-       div = document.createElement("div");
-       
-       
+for (let i = 0; i < n; i++) {
+  if (i == 0) {
+    div = document.createElement("div");
 
+    spanstar = document.createElement("span");
+    spanstar.setAttribute("class", "star");
+    const textnode2 = document.createTextNode("*");
+    spanstar.appendChild(textnode2);
 
-      
-       spanstar = document.createElement("span");
-       spanstar.setAttribute("class", "star")
-       const textnode2 = document.createTextNode("*");
-       spanstar.appendChild(textnode2);
-       
-       div.appendChild(space((length - (2 * i + 1)) / 2))
-       div.appendChild(spanstar)
-       div.appendChild(space((length - (2 * i + 1)) / 2))
-       maindiv.appendChild(div)
-       body.appendChild(maindiv)
+    div.appendChild(space((length - (2 * i + 1)) / 2));
+    div.appendChild(spanstar);
+    div.appendChild(space((length - (2 * i + 1)) / 2));
+    maindiv.appendChild(div);
+    body.appendChild(maindiv);
+  } else {
+    div = document.createElement("div");
 
+    spanstarleft = document.createElement("span");
+    spanstarleft.setAttribute("class", "star");
+    const textnode2 = document.createTextNode("*");
+    spanstarleft.appendChild(textnode2);
 
-       //console.log(empty + star + empty)
-    }
-    
-    else{
+    spanstarright = document.createElement("span");
+    spanstarright.setAttribute("class", "star");
+    const textnode3 = document.createTextNode("*");
+    spanstarright.appendChild(textnode3);
 
-        //star = '*'
-        //line = '|'.repeat(1 + 2 * (i - 1))
-
-        
-       div = document.createElement("div");
-       
-
-
-      
-       spanstarleft = document.createElement("span");
-       spanstarleft.setAttribute("class", "star")
-       const textnode2 = document.createTextNode("*");
-       spanstarleft.appendChild(textnode2);
-
-       spanstarright = document.createElement("span");
-       spanstarright.setAttribute("class", "star")
-       const textnode3 = document.createTextNode("*");
-       spanstarright.appendChild(textnode3);
-       
-       div.appendChild(space((length - (2 * i + 1)) / 2))
-       div.appendChild(spanstarleft)
-       div.appendChild(line(1 + 2 * (i - 1)))
-       div.appendChild(spanstarright)
-       div.appendChild(space((length - (2 * i + 1)) / 2))
-       maindiv.appendChild(div)
-       body.appendChild(maindiv)
-       
-        //console.log(empty + star + line + star + empty)
-    }
-
-    
-    
-    
-
-
-    
+    div.appendChild(space((length - (2 * i + 1)) / 2));
+    div.appendChild(spanstarleft);
+    div.appendChild(line(1 + 2 * (i - 1)));
+    div.appendChild(spanstarright);
+    div.appendChild(space((length - (2 * i + 1)) / 2));
+    maindiv.appendChild(div);
+    body.appendChild(maindiv);
+  }
 }
-
