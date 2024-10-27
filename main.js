0ba07bb27c2fa486cc07430609cdbaac5c2986ed
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-27 20:40:23 +0200

    Manipulate the DOM

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-var n = 4
+var n = prompt("Enter a number")
 var star = ""
 var line = ""
 var empty = ""
 var length = 1 + 2 * (n - 1)
+
+
+
+
+body = document.getElementById("body");
 
 for (let i = 0; i < n; i++){
     empty =" ".repeat((length - (2 * i + 1)) / 2)
     if (i == 0){
        star = '*'
        line = ""
-       console.log(empty+star + empty)
+       div = document.createElement("div");
+       spanempty1 = document.createElement("span");
+       const textnode1 = document.createTextNode(empty);
+       spanempty1.appendChild(textnode1);
+       spanstar = document.createElement("span");
+       const textnode2 = document.createTextNode(star);
+       spanstar.appendChild(textnode2);
+       spanempty3 = document.createElement("span");
+       const textnode3 = document.createTextNode(empty);
+       spanempty3.appendChild(textnode3);
+       div.appendChild(spanempty1)
+       div.appendChild(spanstar)
+       div.appendChild(spanempty3)
+       body.appendChild(div)
+
+
+       //console.log(empty + star + empty)
     }
     
     else{
 
         star = '*'
         line = '|'.repeat(1 + 2 * (i - 1))
-        console.log(empty + star + line + star + empty)
+        //console.log(empty + star + line + star + empty)
     }
+
+    
+    
+    
+
+
     
 }
+
